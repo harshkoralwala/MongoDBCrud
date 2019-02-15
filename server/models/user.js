aa30ebd4cbb09b6a2cf14992b1098ed380e965c7
HARSH KORALWALA <harsh.koralwala@i-link.co.in>
2019-02-15 23:06:10 +0530

    add getr /user/me and authentcatin middleware

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,27 @@ UserSchema.methods.generateAuthToken = function () {
 }
 
 
+
+UserSchema.statics.findByToken = function (token) {
+    console.log("!!!!!!!!!!!!!!!!!");
+    var User = this;
+    var decoded;
+
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+
+
+}
+
 var userModel = new mongoose.model('userModel', UserSchema);
 
 module.exports = { userModel }
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { userModel } = require('./models/user');
 var { ObjectID } = require('mongodb')
-
+var { authenticate } = require("./middleware/authenticate")
 var app = express();
 const port = process.env.port || 3000;
 app.use(bodyParser.json());
@@ -92,7 +92,6 @@ app.post("/users", (req, res) => {
     var userBody = _.pick(req.body, ["email", "password"]);
     var user = new userModel(userBody);
 
-
     user.save().then((userRes) => {
         return user.generateAuthToken()
     }).then((token) => {
@@ -101,6 +100,12 @@ app.post("/users", (req, res) => {
         res.status(400).send(err);
     })
 })
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
+});
+
 
 
 app.listen(port, () => {
