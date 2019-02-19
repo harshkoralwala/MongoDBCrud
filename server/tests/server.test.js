9f37959d63943612d440938d8992c4dec31f20a9
HARSH KORALWALA <harsh.koralwala@i-link.co.in>
2019-02-19 21:30:29 +0530

    add tests for delete /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -274,4 +274,22 @@ describe("post /users/login", () => {
     })
 })
 
+describe("DELETE /users/me/token", () => {
+    it("should remove auth token on logout", (done) => {
 
+        request(app)
+            .delete("/users/me/token")
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                userModel.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((err) => done(err));
+            })
+    })
+});
